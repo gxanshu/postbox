c0e469f05dcbe9a0c0da99a8593324c04491a981
Anshu <gxanshu@yahoo.com>
2026-07-18 14:01:38 +0530

    fix: create db file in usr data dir

--- src/postbox/core/database.py
+++ src/postbox/core/database.py
@@ -1,10 +1,16 @@
+import os
 import sqlite3
+
+from gi.repository import GLib
 
 class Database:
     def __init__(self):
-        conn = sqlite3.connect("/home/anshu/postbox.db")
+        dir = GLib.get_user_data_dir()
+        os.makedirs(dir, exist_ok=True)
+
+        conn = sqlite3.connect(os.path.join(dir, "postbox.db"))
         cursor = conn.cursor()
-        print(cursor.execute("SELECT 1"))
+        cursor.execute("SELECT 1")
         print(cursor.fetchone())
         conn.close()
 
